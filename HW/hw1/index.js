0368e73a4b71bb5c9129d8c99b78abd938a113c2
Olego0 <ostrovskij.013g@gmail.com>
2019-11-15 21:16:20 +0300

    HW

--- HW/hw1/index.js
+++ HW/hw1/index.js
@@ -2,29 +2,33 @@ const adminLogin = 'admin';
 const adminPassword = 'm4ngo1zh4ackz0r';
 const a = prompt("enter, your login:");
 console.log(a);
-if (adminLogin === a) {
-    const b = prompt("enter your password:");
-
-    if (adminPassword === b) {
-        alert ('Добро пожаловать!');}
-    else if(adminPassword != b){
-        alert ("Доступ запрещен, неверный пароль!");}
-    else(adminPassword === null){
-        alert('Отменено пользователем!');}
-    }
-else if(adminLogin != a){
-    alert ("Доступ запрещен, неверный пароль!");
-}
-else if(adminLogin === null){
+if (a === null) {
     alert('Отменено пользователем!');
 }
-    
+if (adminLogin === a) {
+    const b = prompt("enter your password:");
+    if (b === null) {
+        alert('Отменено пользователем!');
+    }
+    if (adminPassword === b) {
+        alert ('Добро пожаловать!');
+    }
+    else{
+        alert ("Доступ запрещен, неверный пароль!");}
+    } 
+else{
+    alert ("Доступ запрещен, неверный пароль!");
+}
 
 
 
     
 
+
+
     
+
+
 
 
 
